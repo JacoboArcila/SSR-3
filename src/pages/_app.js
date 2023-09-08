d0f06a1caec993b7b340f68f97d4b93f3d6ebf74
jacoboArcila <wagner.arcila.jacobo@gmail.com>
2023-09-08 15:52:20 -0500

    theme

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,5 @@
 import "@styles/globals.css";
 import React, { Suspense } from "react";
-import { ThemeProvider } from "styled-components";
-import { theme } from "../../Theme";
 import Layout from "@components/layout/Layout";
 import Loader from "@components/loader/Loader";
 import { ProviderAuth } from "@/hooks/useAuth";
@@ -9,23 +7,19 @@ import { ProviderAuth } from "@/hooks/useAuth";
 export default function App({ Component, pageProps }) {
 	if (Component.getLayout) {
 		return (
-			<ThemeProvider theme={theme}>
-				<ProviderAuth>
-					<Suspense fallback={<Loader />}>{Component.getLayout(<Component {...pageProps} />)}</Suspense>
-				</ProviderAuth>
-			</ThemeProvider>
+			<ProviderAuth>
+				<Suspense fallback={<Loader />}>{Component.getLayout(<Component {...pageProps} />)}</Suspense>
+			</ProviderAuth>
 		);
 	}
 
 	return (
-		<ThemeProvider theme={theme}>
-			<ProviderAuth>
-				<Suspense fallback={<Loader />}>
-					<Layout>
-						<Component {...pageProps} />
-					</Layout>
-				</Suspense>
-			</ProviderAuth>
-		</ThemeProvider>
+		<ProviderAuth>
+			<Suspense fallback={<Loader />}>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</Suspense>
+		</ProviderAuth>
 	);
 }
